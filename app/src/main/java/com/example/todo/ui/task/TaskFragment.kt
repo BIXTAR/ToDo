74caedc3cca1cb6f74c64558d668133dcf41cb94
BIXTAR <daniil3614@gmail.com>
2025-06-20 22:12:06 +0300

    Создание задач фрагментов

---
+++ app/src/main/java/com/example/todo/ui/task/TaskFragment.kt
@@ -0,0 +1,62 @@
+package com.example.todo.ui.task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.todo.R
+import com.example.android.todo.databinding.FragmentTaskBinding
+import com.example.android.todo.viewmodel.TaskViewModel
+
+class TaskFragment {
+    private val viewModel: TaskViewModel by viewModels()
+    private lateinit var adapter: TaskAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentTaskBinding.inflate(inflater)
+
+        binding.lifecycleOwner
+        binding.viewModel = viewModel
+
+        adapter = TaskAdapter(TaskClickListener { task ->
+            findNavController().navigate(TaskFragmentDirections.actionTaskFragmentToUpdateFragment(task))
+        })
+
+        viewModel.getAllTasks.observe(viewLifecycleOwner){
+            adapter.submitList(it)
+        }
+
+        binding.apply {
+
+            binding.recyclerView.adapter = adapter
+
+            floatingActionButton.setOnClickListener{
+                findNavController().navigate(R.id.action_taskFragment_to_addFragment)
+            }
+        }
+        ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val task = adapter.currentList[position]
+                viewModel.delete(task)
+            }
+        }).attachToRecyclerView(binding.recyclerView)
+        return binding.root
+    }
+}
